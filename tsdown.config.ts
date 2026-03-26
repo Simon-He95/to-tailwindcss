80b8928549010a0246736fcc5e6a55ffff131679
Simon He <674949287@qq.com>
2026-03-26 21:48:03 +0800

    chore: update package.json and tsdown.config.ts for improved dependency management

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,21 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   target: 'node14',
   format: ['cjs'],
+  alias: {
+    'transform-to-tailwindcss': resolve('node_modules/transform-to-tailwindcss/dist/index.mjs'),
+  },
   external: [
     'vscode',
+  ],
+  noExternal: [
+    '@typescript-eslint/typescript-estree',
+    'fast-glob',
+    /^svelte(?:\/.*)?$/,
+    'transform-to-tailwindcss',
+    'transform-to-tailwindcss-core',
   ],
   // minify: true,
   clean: true,
